79ce23302d4bef04c5686dbb451f638a189d9cb0
brianpatrickhummel <frankgucci@mac.com>
2017-05-04 16:42:44 -0500

    near final updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,10 @@
 
 
 $(document).ready(function() {
+	
+
+//---------------variable and function declarations------------------
+
 
 	var wins = 0;
 	var losses = 0;
@@ -9,17 +13,21 @@ $(document).ready(function() {
  	var gemValues = [];
  	var images = ['assets/images/1.png', 'assets/images/2.png', 'assets/images/3.png', 'assets/images/4.png'];
   	
+  	
 
+	// define function which resets variables at start/reset, generates target number, generates crystal values and assigns to images
   	function startGame() {
   		$(".gemPics").remove();
-  		totalScore = 0;
-  		$("#score").text(totalScore);
+  		$(".picWin").remove();
+  		$(".picLose").remove();
   		gemValues = [];
-  		console.log(totalScore);
-  		console.log(gemValues);
+  		totalScore = 0;
+  		$("#score").text(totalScore);   // resets score to zero in DOM
   		targetNumber = 19 + Math.floor(Math.random() * 100);
-  		$("#randomNumber").text(targetNumber);
-  		console.log(targetNumber);
+  		$("#randomNumber").text(targetNumber);  // sets new target number in DOM
+  		// console.log(totalScore);
+  		// console.log(gemValues);
+  		// console.log(targetNumber);
 
 		// generate 4 random numbers and push them to the gemValues array
   		for (var i = 0; i < 4; i++) {
@@ -37,38 +45,51 @@ $(document).ready(function() {
 		}
   	}
   
-  	
+
+
+	// define function that process each crystal click to see if change in total score will end the game
   	function checkGuess() {
 	  	if (totalScore === targetNumber) {	
-	  		alert("you win, your score is exactly " + totalScore + " !");
+	  		var imageWin = $('<img>');
+	  		imageWin.addClass('picWin');
+	  		imageWin.attr('src', 'assets/images/win.png');
+	  		imageWin.fadeIn(300).delay(700).fadeOut(700);
+	  		$('body').append(imageWin);
+	  		// alert("you win, your score is exactly " + totalScore + " !");
 	  		wins++;
 	  		$("#wins").text(wins);
-	  		
-	  		console.log(totalScore);
-	  		startGame();
+	  		// console.log(totalScore);
+	  		setTimeout(startGame, 800);
 	  	}
 
 	  	else if (totalScore > targetNumber) {	
-	  		alert("you lose, your score " + totalScore + " is too high");
+	  		var imageLose = $('<img>');
+	  		imageLose.addClass('picLose');
+	  		imageLose.attr('src', 'assets/images/lose.png');
+	  		imageLose.fadeIn(300).delay(700).fadeOut(700);
+	  		$('body').append(imageLose);
 	  		losses++;
 	  		$("#losses").text(losses);
-	  		console.log(totalScore);
-	  		startGame();
+	  		// console.log(totalScore);
+	  		setTimeout(startGame, 800);
 	  	}
   	}
 	
 
 
+
+//--------------main process-------------------------
+
   	startGame();    
-  	$(document).on("click", ".gemPics", function() {     //dynamically created elements and the timing of the on click
-  		alert("I've been clicked");
-  		var crystalValue = ($(this).attr("data-crystalvalue"));
+  	$(document).on("click", ".gemPics", function() {     // selects document rather than <img> since the img elements are dynamically loaded
+  		// alert("I've been clicked");
+  		var crystalValue = ($(this).attr("data-crystalvalue"));  // pulls the assigned crystal value of the image clicked 
     	crystalValue = parseInt(crystalValue);
-    	console.log(crystalValue);
+    	// console.log(crystalValue);
   		totalScore += crystalValue;
   		$("#score").text(totalScore);
-  		console.log(totalScore);
-  		setTimeout(function() { checkGuess(); }, 500);
+  		// console.log(totalScore);
+  		setTimeout(function() { checkGuess(); }, 250);   // delays checkGuess function long enough for the final totalScore update to display in DOM before being reset to zero
   	 });
 
 });
